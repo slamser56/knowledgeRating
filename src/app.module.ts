d0865ffbb323825fee214bb6add3264d64084f30
slamser56 <slamser56@gmail.com>
2021-06-21 06:19:22 +0300

    add mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_DB_URI)],
   controllers: [UsersController],
   providers: [UsersService],
 })
