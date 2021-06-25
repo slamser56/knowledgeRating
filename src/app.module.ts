3993b925272e91728c79769573d9cafbfe54db83
slamser56 <slamser56@gmail.com>
2021-06-25 12:27:57 +0300

    add docker and github workflows

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_DB_URI), UsersModule],
+  controllers: [AppController],
 })
 export class AppModule {}
