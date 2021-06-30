9884d70864dea1476ffdc418f4bba8c73dda49b2
slamser56 <slamser56@gmail.com>
2021-06-30 08:37:25 +0300

    fix app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get('healthcheck')
-  findAll(): string {
+  healthcheck(): string {
     return 'ok';
   }
 }
